38de2641add4f9bda279770cd2d483899004913c
Alain2020 <favouralain@yahoo.com>
2023-11-21 12:38:15 +0000

    Corrected the attribute name from 'nmae' to 'name' in the ExpenseItem component.

---
+++ budget-tracker/src/components/ExpenseList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpenseList = () => {
+  const expenses = [
+    { id: 1231232, name: 'Marketing', cost: 50 },
+    { id: 1231233, name: 'Finance', cost: 300 },
+    { id: 1231234, name: 'Sales', cost: 70 },
+    { id: 1231235, name: 'Human Resource', cost: 50 },
+    { id: 1231236, name: 'IT', cost: 50 }
+  ];
+
+  return (
+    <ul>
+      {expenses.map(expense => (
+        <ExpenseItem
+          key={expense.id}  // Added a unique key for each ExpenseItem
+          id={expense.id}
+          name={expense.name}  // Corrected the attribute name from 'nmae' to 'name'
+          cost={expense.cost}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpenseList;
