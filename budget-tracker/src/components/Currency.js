6c7fb3484584afe476ca0bde819ee41bed453091
Alain2020 <favouralain@yahoo.com>
2023-11-25 01:34:24 +0000

    style: Update Currency component styles - Set the text color of the Currency button to white. - Change the background color of the Currency button to #34A853 when the dropdown is open. - Adjust the background color of the dropdown menu to #34A853.

--- budget-tracker/src/components/Currency.js
+++ budget-tracker/src/components/Currency.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Currency = () => {
-    return (
-       <div className='alert p-4 alert-success' style={{ backgroundColor: 'green', color: 'white' }}>
-        <span>Currency (£ Pound)▾</span>
-       </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedCurrency, setSelectedCurrency] = useState("£ Pound");
+  const currencies = ["$ Dollar","£ Pound", "€ Euro", "₹ Rupee"];
+
+  const handleCurrencyChange = (currency) => {
+    setSelectedCurrency(currency);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className='alert p-3 alert-success' style={{ backgroundColor: isOpen ? '#01DF74' : '' }}>
+      <div className="dropdown">
+        <button
+          className={`btn btn-green dropdown-toggle ${isOpen ? 'active' : ''}`}
+          type="button"
+          id="currencyDropdown"
+          data-bs-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          onClick={() => setIsOpen(!isOpen)}
+          style={{ color: 'white' }}
+        >
+          Currency ({selectedCurrency})
+        </button>
+        <div className={`dropdown-menu ${isOpen ? 'show' : ''}`} aria-labelledby="currencyDropdown" style={{ backgroundColor: '#01DF74' }}>
+          {currencies.map((currency, index) => (
+            <button
+              key={index}
+              className="dropdown-item"
+              onClick={() => handleCurrencyChange(currency)}
+            >
+              {currency}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Currency;
